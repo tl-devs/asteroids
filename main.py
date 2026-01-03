e9a0cd460effa322b640ff75b6cc303ea2dc3507
TL <tl-devs@pm.me>
2026-01-03 14:10:54 +0100

    follow-up

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import pygame as pg
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
 
 def main():
+    pg.init()
     print(f"Starting Asteroids with pygame version: {pg.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+
+        screen.fill(color="black")
+        pg.display.flip()
 
 if __name__ == "__main__":
     main()
