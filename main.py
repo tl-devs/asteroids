616981150ed7b9b9d302d42b9e2821b2a3f37999
TL <tl-devs@pm.me>
2026-01-03 14:04:17 +0100

    follow-up

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame as pg
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
     print(f"Starting Asteroids with pygame version: {pg.version.ver}")
-
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
