961b1a5c35615421a9c9acbd348e50289778a36c
TL <tl-devs@pm.me>
2026-01-04 20:05:43 +0100

    fill-up commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     pg.init()
@@ -9,6 +10,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pg.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -18,7 +21,13 @@ def main():
                 return
 
         screen.fill(color="black")
+
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        player.draw(screen)
+
         pg.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
